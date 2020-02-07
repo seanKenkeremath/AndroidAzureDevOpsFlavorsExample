8ab61108f8ff6bf4dd401f81951a5810d38b3428
Sean Kenkeremath <sean.kenkeremath@willowtreeapps.com>
2020-02-07 14:58:48 -0500

    test adding dynamic gradle build property

--- app/src/main/java/com/seank/azuredevopstest/MainActivity.kt
+++ app/src/main/java/com/seank/azuredevopstest/MainActivity.kt
@@ -5,6 +5,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,5 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView: TextView = findViewById(R.id.domain_name)
+        textView.text = BuildConfig.DOMAIN_NAME
     }
 }
